941e47c8889391ab984d85409e289166060db12e
YeshwanthKare <yesh.kare@gmail.com>
2021-03-23 16:59:36 +0530

    added login and register

--- index.js
+++ index.js
@@ -1,11 +1,99 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose');
+const dotenv = require("dotenv").config()
+const { Schema } = mongoose;
+
+
+app.use(express.json());
+
+mongoose.connect(process.env.dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+})
+.then((res) => {
+    app.listen(port, () => {
+        console.log(`Listening to http://localhost:${port}`);
+    })
+    console.log('DB connected')
+})
+.catch((err) => {
+    console.log(err);
+})
+
+
+const User = mongoose.model('Users', new Schema(
+    { 
+        name: String,
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true
+        }
+    }
+))
+
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 })
 
-app.listen(port, () => {
-    console.log(`Listening to http://localhost:${port}`);
+
+app.post('/register', (req, res) => {
+    const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    })
+    newUser.save((err, user) => {
+        if(err){
+            console.log(err);
+            res.send(400, {
+                status: err
+            })
+        }else{
+            res.send("registered")
+        }
+        console.log('all is good')
+        console.log(user);
+    })
 })
+
+
+app.post('/login', (req, res) => {
+    const password = req.body.password;
+    const email = req.body.email;
+    User.findOne({ email: email, password: password }, (err, user) => {
+        if(user){
+            res.send({
+                status: "valid"
+            })
+            console.log(user)
+        }else{
+            res.send(404,{
+                status: "Not Found"
+            })
+            console.log(err);
+        }
+    })
+    // const validUser = {
+    //     email: 'newmail@mail.com',
+    //     password: 'mypassword'
+    // }
+
+    // if(email == validUser.email && password == validUser.password){
+    //     res.send({
+    //         status: 'Valid'
+    //     })
+    // }else {
+    //     res.status(404).send('Not found');
+    // }
+})
+
